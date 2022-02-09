5753ac06015ce52adac0a3e2ee7b33105a6a1c49
ElenaLj <elenaljubijankic@gmail.com>
2022-02-09 15:27:13 +0100

    add database seeder

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $items = config("comics");
+
+        foreach( $items as $item ) {
+            $newComic = new Comic();
+            $newComic->title = $item["title"];
+            $newComic->description = $item["description"];
+            $newComic->thumb = $item["thumb"];
+            $newComic->price = $item["price"];
+            $newComic->series = $item["series"];
+            $newComic->sale_date = $item["sale_date"];
+            $newComic->type = $item["type"];
+            $newComic->save();
+        }
+    }
+}
