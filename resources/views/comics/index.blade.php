8bdfabca77ac97559ab343ee763e41508e7c2a1a
ElenaLj <elenaljubijankic@gmail.com>
2022-02-09 16:07:12 +0100

    insert comics infos in index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,6 +3,11 @@
     @foreach ($comics as $comic)
     <li>
         <p>{{$comic->title}}</p>
+        <p>{{$comic->price}}</p>
+        <p>{{$comic->series}}</p>
+        <p>{{$comic->type}}</p>
+        <p>{{$comic->description}}</p>
+        <img src="{{asset("$comic->thumb")}}" alt="{{$comic->title}}">
     </li>
     @endforeach
 </ul>
