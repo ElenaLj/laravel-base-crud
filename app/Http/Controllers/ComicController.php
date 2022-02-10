7f063afd0bf81879a7d1fe6bcf0ad56c04bb6655
ElenaLj <elenaljubijankic@gmail.com>
2022-02-10 14:59:02 +0100

    dependency injection attempt

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -45,9 +45,9 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Comic $comics)
     {
-        $comics = Comic::find($id);
+        //$comics = Comic::find($id);
         return view("comics.show", compact("comics"));
     }
 
